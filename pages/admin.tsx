b879247c2baf603bc2640c74eb76ed3015be9743
Robert Matthew Mulder <rmmulder@gmail.com>
2020-12-11 05:50:29 -0500

    Save updates from https://github.com/tobkle/nextjs-netlifycms

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,12 +1,16 @@
 import dynamic from 'next/dynamic';
 import config from '../cms/config';
+// @ts-ignore
 const CMS = dynamic(
+  // @ts-ignore
   () =>
     import('netlify-cms-app').then((cms) => {
+      // @ts-ignore
       cms.init({ config });
     }),
   { ssr: false, loading: () => <p>Loading...</p> }
 );
+
 const AdminPage: React.FC = () => {
   return <CMS />;
 };
